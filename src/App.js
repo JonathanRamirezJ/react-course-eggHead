06f85e4df2969c562bc69fa6cf26014695948a82
JonathanRamirez <jonajramirez@gmail.com>
2018-06-09 14:29:16 -0500

    js: Fourth Lesson

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component{
   render(){
+    let text = this.props.text;
+    let cat = this.props.cat;
     return(
       <div>
-        <h1>hello world 1</h1>
-        <h2>Hello world 2</h2>
-        <h3>Hello world 3</h3>
+        <h1>{text}, {cat}</h1>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  text: PropTypes.string,
+  cat: PropTypes.number
+}
+
+App.defaultProps = {
+  text: "This is default props",
+  cat: 2
 }
 
 export default App;
