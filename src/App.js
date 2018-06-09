73c60ea25b5b04506ea8fc39f5bd3a155bbb7334
JonathanRamirez <jonajramirez@gmail.com>
2018-06-09 13:43:16 -0500

    js: Third lession

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react';
 class App extends Component{
   render(){
     return(
-      <h1>hello world 3</h1>
+      <div>
+        <h1>hello world 1</h1>
+        <h2>Hello world 2</h2>
+        <h3>Hello world 3</h3>
+      </div>
     );
   }
 }
