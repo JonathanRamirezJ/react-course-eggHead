f2ac9fd7f33e075006262857609a2ccf58f4429a
JonathanRamirez <jonajramirez@gmail.com>
2018-06-09 19:30:49 -0500

    js: Seventh Lesson

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 
-const Widget = (props) => {
-  return(
-    <input type="text" onChange={props.update}/>
-  );
-}
-
-class App extends Component{
-  constructor(){
-  	super();
-  	this.state = {
-      text: 'this is state of text',
-      cat: 5
-    };
-  }
-
-  update(e){
-    this.setState({text: e.target.value})
-  }
-
-  render(){
-    let text = this.state.text;
-    let cat = this.state.cat;
-    return(
-      <div>
-        <Widget update={this.update.bind(this)}/>
-        <Widget update={this.update.bind(this)}/>
-        <Widget update={this.update.bind(this)}/>
-        <h1>{text} - {cat}</h1>
-      </div>
+export default class App extends Component {
+  render() {
+    return (
+      <Button>I <Heart/> React</Button>
     );
   }
 }
 
-export default App;
+const Button = (props) => {
+  return(
+    <button>{props.children}</button>
+  );
+}
+
+class Heart extends Component {
+  render() {
+    return (
+      <span>&hearts;</span>
+    );
+  }
+}
